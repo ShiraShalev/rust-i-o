fcce949a12d62315f22f0a65cd8f6abeb8d77314
Shira Shalev <shira@sodot.dev>
2025-02-16 14:57:14 +0200

    pr fixes

--- src/libs.rs
+++ src/libs.rs
@@ -1,6 +1,4 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 
 pub struct Config {
     pub query: String,
@@ -9,9 +7,11 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("Not enough arguments");
+        } else if args.len() > 3 {
+            return Err("Unknown argument provided");
         }
 
         let query = args[1].clone();
@@ -24,81 +24,5 @@ impl Config {
             file_path,
             not_case_sensitive,
         })
-    }
-}
-
-pub fn read_from_file(file_path: &String) -> Result<String, Box<dyn Error>> {
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
-    Ok(contents)
-}
-
-pub fn get_lines_with_query(
-    file_content: &String,
-    query: &String,
-    not_case_sensitive: &bool,
-) -> Vec<String> {
-    let mut results: Vec<String> = Vec::new();
-
-    for line in file_content.lines() {
-        if (!not_case_sensitive && line.contains(query))
-            || (*not_case_sensitive && line.to_lowercase().contains(&query.to_lowercase()))
-        {
-            println!("{line}");
-            results.push(line.to_string());
-        }
-    }
-    results
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn find_string() {
-        let file_content = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape."
-            .to_string();
-        let query = String::from("three");
-        assert_eq!(
-            get_lines_with_query(&file_content, &query, &false),
-            ["Pick three.".to_string()]
-        );
-    }
-
-    #[test]
-    fn find_string_not_cs() {
-        let file_content = "\
-Rust:
-safe, fast, productive, pick.
-Pick three.
-Duct tape."
-            .to_string();
-        let query = String::from("pick");
-        assert_eq!(
-            get_lines_with_query(&file_content, &query, &true),
-            [
-                "safe, fast, productive, pick.".to_string(),
-                "Pick three.".to_string(),
-            ]
-        );
-    }
-
-    #[test]
-    fn dont_find_string() {
-        let file_content = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape."
-            .to_string();
-        let query = String::from("worlds");
-        assert_eq!(
-            get_lines_with_query(&file_content, &query, &false),
-            Vec::<String>::new()
-        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod libs;
 use libs::Config;
-use libs::{get_lines_with_query, read_from_file};
 use std::env;
+use std::fs;
+use std::io::{self, BufRead};
 use std::process;
 
 fn main() {
@@ -10,13 +11,81 @@ fn main() {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    match read_from_file(&config.file_path) {
-        Ok(contents) => {
-            get_lines_with_query(&contents, &config.query, &config.not_case_sensitive);
-        }
-        Err(e) => {
-            eprintln!("Application error: {e}");
-            process::exit(1);
-        }
+
+    if let Err(e) = run(config) {
+        eprint!("Application error: {e}");
+        process::exit(1)
+    }
+}
+fn run(config: Config) -> Result<Box<[String]>, Box<dyn std::error::Error>> {
+    let file = fs::File::open(config.file_path)?;
+    let reader = io::BufReader::new(file);
+    let result = reader
+        .lines()
+        .filter_map(Result::ok)
+        .filter(|line| {
+            let match_found = if config.not_case_sensitive {
+                line.to_lowercase().contains(&config.query.to_lowercase())
+            } else {
+                line.contains(&config.query)
+            };
+            if match_found {
+                println!("{}", line);
+            }
+            match_found
+        })
+        .collect();
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn find_string() {
+        let query = String::from("how");
+        let result: Box<[String]> = Box::new([]);
+        assert_eq!(
+            run(Config {
+                query,
+                file_path: "poem.txt".to_string(),
+                not_case_sensitive: false
+            })
+            .unwrap(),
+            result
+        );
+    }
+
+    #[test]
+    fn find_string_not_cs() {
+        let query = String::from("how");
+        let result: Box<[String]> = Box::new([
+            "How dreary to be somebody!".to_string(),
+            "How public, like a frog".to_string(),
+        ]);
+        assert_eq!(
+            run(Config {
+                query,
+                file_path: "poem.txt".to_string(),
+                not_case_sensitive: true
+            })
+            .unwrap(),
+            result
+        );
+    }
+
+    #[test]
+    fn dont_find_string() {
+        let query = String::from("how");
+        let result: Box<[String]> = Box::new([]);
+        assert_eq!(
+            run(Config {
+                query,
+                file_path: "poem.txt".to_string(),
+                not_case_sensitive: false
+            })
+            .unwrap(),
+            result
+        );
     }
 }
